32cd86eb42fda96ea4d9eb68f96b9d912e4309b4
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-10-31 14:42:40 -0600

    Update main.js

--- main.js
+++ main.js
@@ -1,1 +1,11 @@
-// JavaScript Code
+// SIMPLE ADDITION IPO CALCULATOR BY MR.F
+
+//input
+let number1 = +prompt("Enter a number!");
+let number2 = +prompt("Enter another number")
+
+// Process
+let total = number1 + number2;
+
+// Output
+alert(total);
