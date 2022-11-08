35bf6b5463dbe0f9d974bf85495f644b8d9726cd
Bigkid6999 <112982143+Bigkid6999@users.noreply.github.com>
2022-11-08 14:39:03 -0700

    Update main.js

--- main.js
+++ main.js
@@ -2,10 +2,10 @@
 
 //input
 let number1 = +prompt("Enter a number!");
-let number2 = +prompt("Enter another number")
+let number2 = +prompt("Enter another number");
 
 // Process
 let total = number1 + number2;
 
 // Output
-alert(total);
+alert  ($(number1) = $(number2) = $(total) );
